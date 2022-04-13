a281a5c3b5f2fb52c07a90f623e288401bd844ad
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-13 14:25:22 +0530

    Add files via upload

---
+++ range_method.py
@@ -0,0 +1,32 @@
+# n=int(input("Enter any Integer Number:"))
+# for i in range(n):
+    # print(i)
+    # print(i)
+    # print(i)
+    # print("The Loop has been executed.")
+# print("Done")
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(1,n):
+    # print(i)
+# n=int(input("Enter any Integer Number:"))
+# for i in range(25,n):
+    # print(i,end=" ")
+# 
+# n=int(input("Enter any Integer NUmber:"))
+# for i in range(2,n,2):
+    # print(i)
+
+# b=int(input("Enter any Integer Number:"))
+# if(b%2==0):
+    # print(f"{b} Even number")
+# else:
+    # print(f"{b} odd number")
+
+# for i in range(1,5):
+    # print(i*i)
+    
+# n=int(input("Enter the value of n:"))
+# while(n<10):
+    # print("hello",str(n))
+    # n+=1
